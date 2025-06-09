d8dc6cf64c98793b923d25c2789d8a0c2c5ca856
unknown <filipemotasa@hotmail.com>
2025-06-09 18:43:48 +0100

    first study complete

---
+++ src/config.py
@@ -0,0 +1,13 @@
+TOP2_STOCK_BY_SECTOR = [
+    "AAPL","MSFT",
+    "JPM","V",
+    'LLY','UNH',
+    'AMZN','TSLA',
+    'META','GOOGL',
+    'GE','UBER',
+    'COST','WMT',
+    'XOM','CVX',
+    'NEE','SO',
+    'AMT','PLD',
+    'LIN','SHW'
+]
